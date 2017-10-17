64fbbb2c56ffa8201d725aa3fb23aef474770be6
Anton Pogonets <apogonets@readdle.com>
2017-10-17 20:05:19 +0300

    System: Add TZ=America/Los_Angeles to tests target

--- build-android-swift/copy_resources.sh
+++ build-android-swift/copy_resources.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 # Copy resources
-adb shell rm -rf /data/local/tmp/MailCorePackageTests/resources
-adb shell mkdir -p /data/local/tmp/MailCorePackageTests/resources
-adb push ../unittest/data-android/ /data/local/tmp/MailCorePackageTests/resources
+pushd $(realpath $(dirname $0))
+    adb shell rm -rf /data/local/tmp/MailCorePackageTests/resources
+    adb shell mkdir -p /data/local/tmp/MailCorePackageTests/resources
+    adb push ../unittest/data-android/ /data/local/tmp/MailCorePackageTests/resources
+popd
