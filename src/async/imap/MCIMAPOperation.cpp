b92fac2d55a9c96d40ad5591fc1246867407d50f
Viktor Gedzenko <fox@readdle.com>
2020-06-10 15:12:37 +0300

    Revert "Fix: thread-safety crashes related to CIMAPNamespace"
    This reverts commit 590409cfcb3e3c7a89df8c881b6c9190a67dc8e8.

--- src/async/imap/MCIMAPOperation.cpp
+++ src/async/imap/MCIMAPOperation.cpp
@@ -179,7 +179,11 @@ void IMAPOperation::beforeMain()
 void IMAPOperation::afterMain()
 {
     retain();
+#if defined(__ANDROID__)
     performMethodOnCallbackThread((Object::Method) &IMAPOperation::afterMainOnMainThread, NULL, false);
+#else
+    performMethodOnMainThread((Object::Method) &IMAPOperation::afterMainOnMainThread, NULL);
+#endif
 }
 
 void IMAPOperation::afterMainOnMainThread(void * ctx)
