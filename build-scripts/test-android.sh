3a38419095cadaa93045cd755f345682e2fd8a04
Andrew Druk <adruk@readdle.com>
2020-11-18 16:55:28 +0200

    Dev: fix android ci script

--- build-scripts/test-android.sh
+++ build-scripts/test-android.sh
@@ -1,8 +1,46 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 set -e
 
-unset TOOLCHAINS
+export TOOLCHAINS=org.swift.50320190830a
+export SWIFT_ANDROID_ARCH=x86_64
+export EMULATOR_PORT=5559
+export EMULATOR_NAME=ci-test-$EMULATOR_PORT
+export ANDROID_SERIAL=emulator-$EMULATOR_PORT
+export PATH="$PATH:$ANDROID_HOME/emulator/"
+
+function finish {
+    exit_code=$?
+    set +e
+
+    adb emu kill
+
+    cat ndk-stack.log
+    rm -rf ndk-stack.log
+
+    exit $exit_code
+}
+trap finish EXIT
+
+# Create emulator if needed
+if emulator -list-avds | grep -q $EMULATOR_NAME
+then
+    echo "AVD $EMULATOR_NAME already exist"
+else
+    avdmanager create avd -n $EMULATOR_NAME -k "system-images;android-29;google_apis;x86_64" -d "pixel" --abi google_apis/x86_64
+fi
+
+# Start emulator
+emulator -no-window -avd $EMULATOR_NAME -noaudio -port $EMULATOR_PORT -partition-size 4000 > /dev/null &
+
+# Wait until enmulator actually started with timeout 60 sec
+timeout 60 adb wait-for-any-device
+
+# Clear logcat from previous sessions
+adb logcat -c || true
+
+# Start write adb logcat to file
+adb logcat | ndk-stack -sym .build/debug > ndk-stack.log &
 
 # Force clean
 rm -rf .build Package.resolved test.log
@@ -32,6 +70,9 @@ X_ANDROID_TZ="America/Los_Angeles" \
     swift-test --just-run | tee test.log
 
 return_code=${PIPESTATUS[0]}
+
 cat test.log | xcpretty --report junit
 
-exit $return_code
+cat ndk-stack.log
+
+exit ${return_code}
